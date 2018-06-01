c59af98801f2ada1c14a9cec0d999b2ccd340511
ramiroavch <31267624+ramiroavch@users.noreply.github.com>
2018-05-31 22:40:50 -0400

    login v2

--- pagina-web/js/Login_script.js
+++ pagina-web/js/Login_script.js
@@ -3,18 +3,11 @@ function Log_Click(){
 var email=document.getElementById("user_txt").value;
 var pass=document.getElementById("pass_txt").value	
 var boole =0;
-firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
-  // Handle Errors here.
-  	var errorCode = error.code;
-  	var errorMessage = error.message;
-	var boole2=1;
-  	window.alert(errorMessage);
-  
-  // ...
-});
-
-if(boole2==1){
- 	window.alert(boole);
- 	location.href = "Proyectos.html";
-}
+const promise = firebase.auth().signInWithEmailAndPassword(email, pass)
+	.then(user => {
+        location = 'Proyectos.html' //Url aqui
+    }).catch(error => {
+        window.alert(error);
+    });
+    promise.catch(e => window.alert(e.message));
 }
